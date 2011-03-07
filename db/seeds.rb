450f7f4845f8c12344955b5b7950dc8e6aeeb2c9
goozzik <jakub.gorzelak@gmail.com>
2011-03-07 22:00:24 +0100

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@ Banner.create(:name => 'Ad-1', :ads_code => '123asd')
 Zone.create(:name => 'Zone-1') 
 Stat.create(:zone_id => 1, :banner_id => 1,
             :hits => 2, :views => 4, :day => Time.local(2011,3,2))
+
+Banner.create(:name => 'Ad-2', :ads_code => '123asd')
+Zone.create(:name => 'Zone-2') 
+Stat.create(:zone_id => 2, :banner_id => 2,
+            :hits => 3, :views => 4, :day => Time.local(2011,3,2))
+
+Banner.create(:name => 'Ad-3', :ads_code => '123asd')
+Zone.create(:name => 'Zone-3') 
+Stat.create(:zone_id => 3, :banner_id => 3,
+            :hits => 4, :views => 4, :day => Time.local(2011,3,2))
